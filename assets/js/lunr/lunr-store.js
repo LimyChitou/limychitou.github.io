21fa8a08c12800b2b5322b2710c71590b1aa9157
LimyChitou <kalitvenjev@gmail.com>
2022-09-25 18:52:24 +0300

    Test Commit
    Basic Website

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Website Under Development",
+        "excerpt":"   ⚠ Danger:     Poop came out      🖍 Note:     This website is under development.    ","categories": [],
+        "tags": [],
+        "url": "/2022/09/25/development.html",
+        "teaser": null
+      }]
